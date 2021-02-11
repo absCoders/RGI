1e574047898756e1157ba00db05a0dd7a09a5f6a
Wayne Richmond <whr@waynerichmond.net>
2021-02-11 18:14:31 -0500

    WHR - FEFD Parameters for Rich.

--- SO/Tables/SOTPARMR.vb
+++ SO/Tables/SOTPARMR.vb
@@ -1,3 +1,29 @@
 Public Class SOTPARMR
-     
+    Private Sub Form_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        With dst
+            ASCMAIN1.sql = "Select *" _
+                & " from SOTPARM2 " _
+                & " WHERE SO_PARM_KEY = :PARM1"
+            Create_TDA(.Tables.Add, "SOTPARM2", "**", 0, True, "V", 1)
+        End With
+
+    End Sub
+
+    Overrides Sub Show_Record_Special()
+        Fill_Records("SOTPARM2", "Z")
+        If dst.Tables.Item("SOTPARMR").Rows.Count = 1 And dst.Tables.Item("SOTPARM2").Rows.Count = 1 Then
+            For Each UCOL As String In New String() {"SO_PARM_CONCOST", "SO_PARM_DUTY", "SO_PARM_INLANDFRT", "SO_PARM_OCEANFRTCONS", "SO_PARM_OCEANFRT"}
+                dst.Tables.Item("SOTPARMR").Rows(0).Item(UCOL) = dst.Tables.Item("SOTPARM2").Rows(0).Item(UCOL).ToString & String.Empty
+            Next
+        End If
+    End Sub
+    Overrides Sub Proceed_Update_Special_Pre()
+        If dst.Tables.Item("SOTPARMR").Rows.Count = 1 And dst.Tables.Item("SOTPARM2").Rows.Count = 1 Then
+            For Each UCOL As String In New String() {"SO_PARM_CONCOST", "SO_PARM_DUTY", "SO_PARM_INLANDFRT", "SO_PARM_OCEANFRTCONS", "SO_PARM_OCEANFRT"}
+                dst.Tables.Item("SOTPARM2").Rows(0).Item(UCOL) = dst.Tables.Item("SOTPARMR").Rows(0).Item(UCOL).ToString & String.Empty
+            Next
+        End If
+
+        Update_Record_TDA("SOTPARM2")
+    End Sub
 End Class
