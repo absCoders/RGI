df308c7a1b7d7fd3d842b03d6fb157e2f8a0e51d
Rick Ruano <rick@absolution.com>
2021-03-30 19:15:16 -0400

    recount resets verified status

--- IC/Forms/ICFPHYC1.vb
+++ IC/Forms/ICFPHYC1.vb
@@ -919,6 +919,9 @@ Public Class ICFPHYC1
         BeginTrans()
         ASCMAIN1.sql = "Insert into ICTPHYC1_RECNT Values(:PARM1)"
         ASCDATA1.ExecuteSQL(ASCMAIN1.sql, "V", Location)
+
+        ASCMAIN1.sql = "Update ICTPHYC1 set VERIFIED_OPER = '' where LOCATION_CODE = :PARM1"
+        ASCDATA1.ExecuteSQL(ASCMAIN1.sql, "V", Location)
         CommitTrans()
 
         Using ipp As New nsoftware.IPWorks.Ipport
