fd23d0c16bb48a9419aec6b62cda9ba1032e827f
Rick Ruano <rick@absolution.com>
2021-05-17 09:27:33 -0400

    PKG_LABEL Update

--- WH/Forms/WHFLB128.vb
+++ WH/Forms/WHFLB128.vb
@@ -556,6 +556,7 @@ Public Class WHFLB128
 
         Dim ZebraPrinters As New List(Of String)
         If ASCMAIN1.DBS_COMPANY = "VAN" Or ASCMAIN1.DBS_SERVER = "VAN" Then
+            If ASCMAIN1.Running_in_VS Then Stop
             If Not ASCMAIN1.Running_in_VS Then
                 For Each printerName As String In Drawing.Printing.PrinterSettings.InstalledPrinters
                     If printerName.ToUpper.StartsWith("ZDESIGNER") Or printerName.ToUpper.StartsWith("MONARCH") Or printerName.ToUpper.StartsWith("AVERY") Or printerName.ToUpper.StartsWith("ZEBRA") Then
@@ -575,9 +576,9 @@ Public Class WHFLB128
                 Next
             End If
             cboZebraPrinter.DataSource = ZebraPrinters
-            End If
+        End If
 
-            For I As Integer = 0 To tabLabels.Tabs.Count - 1
+        For I As Integer = 0 To tabLabels.Tabs.Count - 1
             If tabLabels.Tabs(I).Text = "UCC128" Then
                 UCC128TAB = I
             End If
@@ -1981,7 +1982,7 @@ Public Class WHFLB128
     Sub Print_UCC128_Labels_for_Selected_Shipments()
         Try
             Dim PrinterName As String
-            If ASCMAIN1.CLIENT = "VAN" And ASCMAIN1.Running_in_VS Then
+            If ASCMAIN1.CLIENT = "VAN" And Not ASCMAIN1.Running_in_VS Then
                 Dim ZebraPrinter As String = cboZebraPrinter.SelectedValue
                 Dim PRINTER_PORT As String = ZebraPrinter.Split("|")(2)
                 PrinterName = PRINTER_PORT
