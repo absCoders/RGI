7fa647f04fde19dd4272192366bd85561e3a11f5
Robert Wall <rdw@absolution.com>
2021-11-03 11:19:07 -0400

    support vendors removing discontinued styles from vendor info sheet

--- IC/Forms/ICFXLSWR.vb
+++ IC/Forms/ICFXLSWR.vb
@@ -1029,8 +1029,10 @@ Public Class ICFXLSWR
 
                 dst.Tables("ICTXLSW3_V").Rows.Clear()
 
+                Dim styleRowOffset As Integer = 0
+
                 For r As Int64 = 2 To ws.UsedRange.RowCount - 1
-                    Dim XLS_IMP_LNO As Integer = r - 1
+                    Dim XLS_IMP_LNO As Integer = r - 1 + styleRowOffset
                     Dim STYLE_CODE As String = ws.Cells(r, 0).Text
 
                     If STYLE_CODE = "" Then Exit For
@@ -1058,6 +1060,7 @@ Public Class ICFXLSWR
                                 Continue For
                             End If
                         Else
+                            styleRowOffset += 1
                             Continue For
                         End If
                     End If
