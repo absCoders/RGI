f061a7d96ec8f513b38c8b5a5ce9c90a75f2d391
rickabs <rick@absolution.com>
2025-01-31 06:40:33 -0500

    New wifi printer server

--- WH/Forms/WHFUPCL1.vb
+++ WH/Forms/WHFUPCL1.vb
@@ -604,9 +604,9 @@ Public Class WHFUPCL1
         Using ipp As New nsoftware.IPWorks.Ipport
             ipp.RuntimeLicense = TACMAIN1.nSoftwareIPWorksV9Key
             If ASCMAIN1.Running_in_VS Then
-                ipp.Connect("192.168.1.19", "4444") 'ipp.Connect("192.168.120.67", "4444") '"192.168.4.117", "4444")
+                ipp.Connect("192.168.50.74", "4444") 'ipp.Connect("192.168.120.67", "4444") '"192.168.4.117", "4444")
             Else
-                ipp.Connect("192.168.110.223", "4444")
+                ipp.Connect("192.168.110.239", "4444")
             End If
             Dim data As String '= "upc123" ' & vbCrLf a new line is needed to send the data across
             Try
