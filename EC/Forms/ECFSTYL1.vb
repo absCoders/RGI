6cc7eca3d743790aa4ad3d4737c6d7f3276c1f72
Wayne Richmond <filter@waynerichmond.net>
2025-03-10 14:10:20 -0400

    Update ECFSTYL1.vb

--- EC/Forms/ECFSTYL1.vb
+++ EC/Forms/ECFSTYL1.vb
@@ -3067,7 +3067,7 @@ Public Class ECFSTYL1
             ASCMAIN1.Progress("Reading File")
             Me.Cursor = Cursors.WaitCursor
 
-            Using cn As New System.Data.OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & fileToImport & ";Extended Properties=""Excel 12.0;HDR=YES;IMEX=1""")
+            Using cn As New System.Data.OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.16.0;Data Source=" & fileToImport & ";Extended Properties=""Excel 12.0;HDR=YES;IMEX=1""")
                 Using cmd As New System.Data.OleDb.OleDbDataAdapter("select * from [Dont Change The Column Names$]", cn)
                     ' Select the data from Sheet1 of the workbook.
                     cn.Open()
