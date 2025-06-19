375b614de7b1f4d585e8b51de9afe65a56e2ffcb
Edwin W Zenker <ewz@absolution.com>
2025-06-19 11:05:10 -0400

    FedEx Rates calculation change

--- TAC/Classes/WHCSHIP1.vb
+++ TAC/Classes/WHCSHIP1.vb
@@ -4099,6 +4099,7 @@ Public Class WHCSHIP1
             objFedexRates.FedExAccount.Password = cPassword
             objFedexRates.FedExAccount.MeterNumber = cFedexMeterNumber
             objFedexRates.RequestedService = cRequestedServiceType
+            objFedexRates.RateType = 3 ' 1=List, 2=Account, 3=List & Account
 
             objFedexRates.Config("SSLEnabledProtocols=" & SSLEnabledProtocols)
             System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 Or SecurityProtocolType.Tls Or SecurityProtocolType.Tls11 Or SecurityProtocolType.Tls12
@@ -4252,10 +4253,7 @@ Public Class WHCSHIP1
 
                     .RecipientAddress.ZipCode = "10007" ' "07081"
                     .RecipientAddress.CountryCode = "US"
-
-                    '.RecipientAddress.AddressFlags = 2
                     .RequestedService = ServiceTypes.stUnspecified
-                    '.PickupType = FedexratesPickupTypes.fptUseScheduledPickup
 
                     Dim PackageDetail As New PackageDetail
                     With PackageDetail
@@ -4346,7 +4344,10 @@ Public Class WHCSHIP1
                 objFedexRates.ShipmentSpecialServices = ShipmentSpecialServices
             End If
 
-            objFedexRates.RateType = &H2 Or &H1 ' FedexratesRateTypes.rtList
+            'objFedexRates.RateType = &H2 Or &H1 ' FedexratesRateTypes.rtList
+            objFedexRates.RateType = 3 ' 1=List, 2=Account, 3=List & Account
+            objFedexRates.PickupType = FedexratesPickupTypes.fptRegularStop
+            objFedexRates.PickupType = FedexratesPickupTypes.fptUseScheduledPickup
 
             ' Geting a resouce error, try it twice. May be the test environment
             Dim success As Boolean = True
@@ -4367,16 +4368,22 @@ Public Class WHCSHIP1
                     With requestedRateList(iLoop)
                         .ServiceType = objFedexRates.Services(iLoop).ServiceType
                         .ServiceTypeDescription = StrConv(objFedexRates.Services(iLoop).ServiceTypeDescription.Replace("_", " "), VbStrConv.ProperCase)
-                        .AccountNetCharge = Val(objFedexRates.Services(iLoop).AccountNetCharge & String.Empty) + Val(objFedexRates.Services(iLoop).AccountTotalSurcharge & String.Empty)
+                        .AccountNetCharge = Val(objFedexRates.Services(iLoop).AccountNetCharge & String.Empty) ' + Val(objFedexRates.Services(iLoop).AccountTotalSurcharge & String.Empty)
                         .DeliveryTime = objFedexRates.Services(iLoop).DeliveryTime
+                        .DeliveryDate = objFedexRates.Services(iLoop).DeliveryDate
                         .ListNetCharge = Val(objFedexRates.Services(iLoop).ListNetCharge & String.Empty)
                         .TransitTime = objFedexRates.Services(iLoop).TransitTime & String.Empty
 
-                        If inTestMode Then
-                            If .AccountNetCharge = 0 Then
-                                .AccountNetCharge = .ListNetCharge
-                            End If
+                        ' See if we have a negotiated rate, if not then use List Net Charge
+                        If .AccountNetCharge = 0 Then
+                            .AccountNetCharge = .ListNetCharge
                         End If
+
+                        'If inTestMode Then
+                        '    If .AccountNetCharge = 0 Then
+                        '        .AccountNetCharge = .ListNetCharge
+                        '    End If
+                        'End If
 
                         .Disclaimer = String.Empty
 
@@ -5244,7 +5251,7 @@ Public Class WHCSHIP1
                     If isGroundFreight AndAlso .ServiceType = "43" Then
                         .ServiceTypeDescription = "UPS Ground Freight"
                     End If
-                    .AccountNetCharge = Val(objUpsRates.Services(iLoop).AccountNetCharge & String.Empty) + Val(objUpsRates.Services(iLoop).AccountTotalSurcharge & String.Empty)
+                    .AccountNetCharge = Val(objUpsRates.Services(iLoop).AccountNetCharge & String.Empty) ' + Val(objUpsRates.Services(iLoop).AccountTotalSurcharge & String.Empty)
                     .DeliveryTime = objUpsRates.Services(iLoop).DeliveryTime
                     .ListNetCharge = Val(objUpsRates.Services(iLoop).ListNetCharge & String.Empty)
                     .TransitTime = objUpsRates.Services(iLoop).TransitTime
