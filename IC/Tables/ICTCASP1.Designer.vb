d238d1d5c0310a9b93ed6f648c14b64d4d60420c
Darrin Joscelyn <dgj@absolution.com>
2025-08-12 13:09:24 -0400

    Add Style Cube Default Table to IC File maintenance

---
+++ IC/Tables/ICTCASP1.Designer.vb
@@ -0,0 +1,289 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ICTCASP1
+    'Inherits System.Windows.Forms.Form
+    Inherits ABSolution.ASFCODEM
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim Appearance5 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
+        Dim Appearance6 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
+        Dim Appearance7 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
+        Dim Appearance8 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
+        Dim Appearance9 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
+        Dim Appearance10 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
+        Dim Appearance11 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
+        Dim Appearance12 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
+        Dim Appearance13 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
+        Dim Appearance4 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
+        Dim Appearance3 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
+        Dim Appearance2 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
+        Dim Appearance1 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
+        Me.UltraLabel1 = New Infragistics.Win.Misc.UltraLabel()
+        Me.UltraTextEditor1 = New Infragistics.Win.UltraWinEditors.UltraTextEditor()
+        Me.UltraTextEditor3 = New Infragistics.Win.UltraWinEditors.UltraTextEditor()
+        Me.UltraLabel3 = New Infragistics.Win.Misc.UltraLabel()
+        Me.UltraNumericEditor4 = New Infragistics.Win.UltraWinEditors.UltraNumericEditor()
+        Me.UltraLabel2 = New Infragistics.Win.Misc.UltraLabel()
+        Me.UltraLabel4 = New Infragistics.Win.Misc.UltraLabel()
+        Me.numCASE_CUBE = New Infragistics.Win.UltraWinEditors.UltraNumericEditor()
+        Me.Panel1.SuspendLayout()
+        CType(Me.tbl, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.UltraExplorerBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ASFBASE1_Fill_Panel.SuspendLayout()
+        CType(Me.grdASFBASEX, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.tlb, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.tblASTOPST1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.tblASFBASE1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.tblASFBASE1_Schema, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dst, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.UltraTextEditor1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.UltraTextEditor3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.UltraNumericEditor4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.numCASE_CUBE, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.numCASE_CUBE)
+        Me.Panel1.Controls.Add(Me.UltraLabel4)
+        Me.Panel1.Controls.Add(Me.UltraLabel2)
+        Me.Panel1.Controls.Add(Me.UltraNumericEditor4)
+        Me.Panel1.Controls.Add(Me.UltraLabel3)
+        Me.Panel1.Controls.Add(Me.UltraTextEditor3)
+        Me.Panel1.Controls.Add(Me.UltraLabel1)
+        Me.Panel1.Controls.Add(Me.UltraTextEditor1)
+        Me.Panel1.Size = New System.Drawing.Size(1019, 575)
+        '
+        'UltraExplorerBar1
+        '
+        Me.UltraExplorerBar1.GroupSettings.UseMnemonics = Infragistics.Win.DefaultableBoolean.[True]
+        Me.UltraExplorerBar1.ItemSettings.Style = Infragistics.Win.UltraWinExplorerBar.ItemStyle.Button
+        Me.UltraExplorerBar1.Margin = New System.Windows.Forms.Padding(5, 3, 5, 3)
+        Me.UltraExplorerBar1.Margins.Bottom = 0
+        Me.UltraExplorerBar1.Margins.Left = 0
+        Me.UltraExplorerBar1.Margins.Right = 0
+        Me.UltraExplorerBar1.Margins.Top = 0
+        Me.UltraExplorerBar1.Size = New System.Drawing.Size(208, 622)
+        '
+        'ASFBASE1_Fill_Panel
+        '
+        Me.ASFBASE1_Fill_Panel.Size = New System.Drawing.Size(1023, 646)
+        '
+        'grdASFBASEX
+        '
+        Appearance5.BackColor = System.Drawing.SystemColors.Window
+        Appearance5.BorderColor = System.Drawing.SystemColors.InactiveCaption
+        Me.grdASFBASEX.DisplayLayout.Appearance = Appearance5
+        Me.grdASFBASEX.DisplayLayout.BorderStyle = Infragistics.Win.UIElementBorderStyle.Solid
+        Me.grdASFBASEX.DisplayLayout.CaptionVisible = Infragistics.Win.DefaultableBoolean.[False]
+        Me.grdASFBASEX.DisplayLayout.GroupByBox.BorderStyle = Infragistics.Win.UIElementBorderStyle.Solid
+        Me.grdASFBASEX.DisplayLayout.MaxColScrollRegions = 1
+        Me.grdASFBASEX.DisplayLayout.MaxRowScrollRegions = 1
+        Appearance6.BackColor = System.Drawing.SystemColors.Window
+        Appearance6.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.grdASFBASEX.DisplayLayout.Override.ActiveCellAppearance = Appearance6
+        Appearance7.BackColor = System.Drawing.SystemColors.Highlight
+        Appearance7.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.grdASFBASEX.DisplayLayout.Override.ActiveRowAppearance = Appearance7
+        Me.grdASFBASEX.DisplayLayout.Override.BorderStyleCell = Infragistics.Win.UIElementBorderStyle.Dotted
+        Me.grdASFBASEX.DisplayLayout.Override.BorderStyleRow = Infragistics.Win.UIElementBorderStyle.Dotted
+        Appearance8.BackColor = System.Drawing.SystemColors.Window
+        Me.grdASFBASEX.DisplayLayout.Override.CardAreaAppearance = Appearance8
+        Appearance9.BorderColor = System.Drawing.Color.Silver
+        Appearance9.TextTrimming = Infragistics.Win.TextTrimming.EllipsisCharacter
+        Me.grdASFBASEX.DisplayLayout.Override.CellAppearance = Appearance9
+        Me.grdASFBASEX.DisplayLayout.Override.CellClickAction = Infragistics.Win.UltraWinGrid.CellClickAction.EditAndSelectText
+        Me.grdASFBASEX.DisplayLayout.Override.CellPadding = 0
+        Appearance10.BackColor = System.Drawing.SystemColors.Control
+        Appearance10.BackColor2 = System.Drawing.SystemColors.ControlDark
+        Appearance10.BackGradientAlignment = Infragistics.Win.GradientAlignment.Element
+        Appearance10.BackGradientStyle = Infragistics.Win.GradientStyle.Horizontal
+        Appearance10.BorderColor = System.Drawing.SystemColors.Window
+        Me.grdASFBASEX.DisplayLayout.Override.GroupByRowAppearance = Appearance10
+        Appearance11.TextHAlignAsString = "Left"
+        Me.grdASFBASEX.DisplayLayout.Override.HeaderAppearance = Appearance11
+        Me.grdASFBASEX.DisplayLayout.Override.HeaderClickAction = Infragistics.Win.UltraWinGrid.HeaderClickAction.SortMulti
+        Me.grdASFBASEX.DisplayLayout.Override.HeaderStyle = Infragistics.Win.HeaderStyle.WindowsXPCommand
+        Appearance12.BackColor = System.Drawing.SystemColors.Window
+        Appearance12.BorderColor = System.Drawing.Color.Silver
+        Me.grdASFBASEX.DisplayLayout.Override.RowAppearance = Appearance12
+        Me.grdASFBASEX.DisplayLayout.Override.RowSelectors = Infragistics.Win.DefaultableBoolean.[False]
+        Appearance13.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.grdASFBASEX.DisplayLayout.Override.TemplateAddRowAppearance = Appearance13
+        Me.grdASFBASEX.DisplayLayout.ScrollBounds = Infragistics.Win.UltraWinGrid.ScrollBounds.ScrollToFill
+        Me.grdASFBASEX.DisplayLayout.ScrollStyle = Infragistics.Win.UltraWinGrid.ScrollStyle.Immediate
+        Me.grdASFBASEX.DisplayLayout.ViewStyleBand = Infragistics.Win.UltraWinGrid.ViewStyleBand.OutlookGroupBy
+        '
+        '_ASFBASE1_Toolbars_Dock_Area_Left
+        '
+        Me._ASFBASE1_Toolbars_Dock_Area_Left.Margin = New System.Windows.Forms.Padding(6, 3, 6, 3)
+        Me._ASFBASE1_Toolbars_Dock_Area_Left.Size = New System.Drawing.Size(0, 646)
+        '
+        '_ASFBASE1_Toolbars_Dock_Area_Right
+        '
+        Me._ASFBASE1_Toolbars_Dock_Area_Right.Location = New System.Drawing.Point(1236, 0)
+        Me._ASFBASE1_Toolbars_Dock_Area_Right.Margin = New System.Windows.Forms.Padding(6, 3, 6, 3)
+        Me._ASFBASE1_Toolbars_Dock_Area_Right.Size = New System.Drawing.Size(0, 646)
+        '
+        '_ASFBASE1_Toolbars_Dock_Area_Top
+        '
+        Me._ASFBASE1_Toolbars_Dock_Area_Top.Margin = New System.Windows.Forms.Padding(6, 3, 6, 3)
+        Me._ASFBASE1_Toolbars_Dock_Area_Top.Size = New System.Drawing.Size(1236, 0)
+        '
+        '_ASFBASE1_Toolbars_Dock_Area_Bottom
+        '
+        Me._ASFBASE1_Toolbars_Dock_Area_Bottom.Location = New System.Drawing.Point(0, 646)
+        Me._ASFBASE1_Toolbars_Dock_Area_Bottom.Margin = New System.Windows.Forms.Padding(6, 3, 6, 3)
+        Me._ASFBASE1_Toolbars_Dock_Area_Bottom.Size = New System.Drawing.Size(1236, 0)
+        '
+        'tlb
+        '
+        Me.tlb.MenuSettings.ForceSerialization = True
+        Me.tlb.ToolbarSettings.ForceSerialization = True
+        '
+        'UltraLabel1
+        '
+        Appearance4.BackColor = System.Drawing.Color.Transparent
+        Me.UltraLabel1.Appearance = Appearance4
+        Me.UltraLabel1.AutoSize = True
+        Me.UltraLabel1.Location = New System.Drawing.Point(20, 51)
+        Me.UltraLabel1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.UltraLabel1.Name = "UltraLabel1"
+        Me.UltraLabel1.Size = New System.Drawing.Size(135, 22)
+        Me.UltraLabel1.TabIndex = 52
+        Me.UltraLabel1.Text = "Customer Code"
+        '
+        'UltraTextEditor1
+        '
+        Me.Absx1.SetABSColumnName(Me.UltraTextEditor1, "CUST_CODE")
+        Me.Absx1.SetABSHasButton(Me.UltraTextEditor1, True)
+        Me.UltraTextEditor1.Location = New System.Drawing.Point(177, 47)
+        Me.UltraTextEditor1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.UltraTextEditor1.Name = "UltraTextEditor1"
+        Me.UltraTextEditor1.Size = New System.Drawing.Size(120, 29)
+        Me.UltraTextEditor1.TabIndex = 0
+        '
+        'UltraTextEditor3
+        '
+        Me.Absx1.SetABSColumnName(Me.UltraTextEditor3, "SUB_BODY_CODE")
+        Me.Absx1.SetABSHasButton(Me.UltraTextEditor3, True)
+        Me.UltraTextEditor3.Location = New System.Drawing.Point(177, 84)
+        Me.UltraTextEditor3.Margin = New System.Windows.Forms.Padding(4)
+        Me.UltraTextEditor3.Name = "UltraTextEditor3"
+        Me.UltraTextEditor3.Size = New System.Drawing.Size(120, 29)
+        Me.UltraTextEditor3.TabIndex = 54
+        '
+        'UltraLabel3
+        '
+        Appearance3.BackColor = System.Drawing.Color.Transparent
+        Me.UltraLabel3.Appearance = Appearance3
+        Me.UltraLabel3.AutoSize = True
+        Me.UltraLabel3.Location = New System.Drawing.Point(20, 88)
+        Me.UltraLabel3.Margin = New System.Windows.Forms.Padding(4)
+        Me.UltraLabel3.Name = "UltraLabel3"
+        Me.UltraLabel3.Size = New System.Drawing.Size(134, 22)
+        Me.UltraLabel3.TabIndex = 55
+        Me.UltraLabel3.Text = "Sub Body Code"
+        '
+        'UltraNumericEditor4
+        '
+        Me.Absx1.SetABSColumnName(Me.UltraNumericEditor4, "CARTON_PACK_QTY")
+        Me.UltraNumericEditor4.AlwaysInEditMode = True
+        Me.UltraNumericEditor4.Location = New System.Drawing.Point(177, 122)
+        Me.UltraNumericEditor4.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.UltraNumericEditor4.MaxValue = 10000
+        Me.UltraNumericEditor4.MinValue = 0
+        Me.UltraNumericEditor4.Name = "UltraNumericEditor4"
+        Me.UltraNumericEditor4.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
+        Me.UltraNumericEditor4.Size = New System.Drawing.Size(70, 29)
+        Me.UltraNumericEditor4.TabIndex = 203
+        '
+        'UltraLabel2
+        '
+        Appearance2.BackColor = System.Drawing.Color.Transparent
+        Me.UltraLabel2.Appearance = Appearance2
+        Me.UltraLabel2.AutoSize = True
+        Me.UltraLabel2.Location = New System.Drawing.Point(24, 163)
+        Me.UltraLabel2.Margin = New System.Windows.Forms.Padding(4)
+        Me.UltraLabel2.Name = "UltraLabel2"
+        Me.UltraLabel2.Size = New System.Drawing.Size(95, 22)
+        Me.UltraLabel2.TabIndex = 204
+        Me.UltraLabel2.Text = "Case Cube"
+        '
+        'UltraLabel4
+        '
+        Appearance1.BackColor = System.Drawing.Color.Transparent
+        Me.UltraLabel4.Appearance = Appearance1
+        Me.UltraLabel4.AutoSize = True
+        Me.UltraLabel4.Location = New System.Drawing.Point(24, 122)
+        Me.UltraLabel4.Margin = New System.Windows.Forms.Padding(4)
+        Me.UltraLabel4.Name = "UltraLabel4"
+        Me.UltraLabel4.Size = New System.Drawing.Size(142, 22)
+        Me.UltraLabel4.TabIndex = 205
+        Me.UltraLabel4.Text = "Carton Pack Qty"
+        '
+        'numCASE_CUBE
+        '
+        Me.Absx1.SetABSColumnName(Me.numCASE_CUBE, "CASE_CUBE")
+        Me.numCASE_CUBE.AlwaysInEditMode = True
+        Me.numCASE_CUBE.Location = New System.Drawing.Point(177, 163)
+        Me.numCASE_CUBE.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.numCASE_CUBE.MaxValue = 10000
+        Me.numCASE_CUBE.MinValue = 0
+        Me.numCASE_CUBE.Name = "numCASE_CUBE"
+        Me.numCASE_CUBE.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
+        Me.numCASE_CUBE.Size = New System.Drawing.Size(96, 29)
+        Me.numCASE_CUBE.TabIndex = 214
+        '
+        'ICTCASP1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1236, 646)
+        Me.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+        Me.Name = "ICTCASP1"
+        Me.Text = "ICTCASP1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.tbl, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.UltraExplorerBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ASFBASE1_Fill_Panel.ResumeLayout(False)
+        CType(Me.grdASFBASEX, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tlb, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tblASTOPST1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tblASFBASE1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tblASFBASE1_Schema, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dst, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.UltraTextEditor1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.UltraTextEditor3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.UltraNumericEditor4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.numCASE_CUBE, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents UltraLabel1 As Infragistics.Win.Misc.UltraLabel
+    Friend WithEvents UltraTextEditor1 As Infragistics.Win.UltraWinEditors.UltraTextEditor
+    Friend WithEvents UltraLabel3 As Misc.UltraLabel
+    Friend WithEvents UltraTextEditor3 As UltraWinEditors.UltraTextEditor
+    Friend WithEvents UltraLabel4 As Misc.UltraLabel
+    Friend WithEvents UltraLabel2 As Misc.UltraLabel
+    Friend WithEvents UltraNumericEditor4 As UltraWinEditors.UltraNumericEditor
+    Friend WithEvents numCASE_CUBE As UltraWinEditors.UltraNumericEditor
+End Class
---
+++ IC/Tables/ICTCASP1.vb
@@ -0,0 +1,5 @@
+Public Class ICTCASP1
+    Private Sub UltraLabel2_Click(sender As Object, e As EventArgs) Handles UltraLabel2.Click
+
+    End Sub
+End Class
