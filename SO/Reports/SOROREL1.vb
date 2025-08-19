3dddfe80666dbf7560deb2c0e914c982951fec57
Walter J. Zielenski <wjz@absolution.com>
2025-08-19 10:36:40 -0400

    allow ecommerce for RGI

--- SO/Reports/SOROREL1.vb
+++ SO/Reports/SOROREL1.vb
@@ -3674,6 +3674,8 @@ Public Class SOROREL1
                 Else
                     If chkAllocateNoRelease.Checked Then
                         ' this is ok
+                    ElseIf chkECommerce.Checked Then
+                        ' this is ok
                     ElseIf Absx1.txtFor("WHSE_CODE").Text = "MS" Then
                         EMsg &= vbCr & "MS Sales Order Release must be done by choosing Manually Selected only"
                     ElseIf Absx1.txtFor("WHSE_CODE").Text = "" Then
