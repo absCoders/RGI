8c82eaeaae9470573f5e50ab455f942e9f9bb9af
rickabs <rick@absolution.com>
2025-10-06 15:31:41 -0400

    Changes to allow processing "T" pallets and pick tickets.

--- WHC/Classes/WHCRF024.vb
+++ WHC/Classes/WHCRF024.vb
@@ -113,7 +113,7 @@
                             End If
                         End If
 
-                        Dim PICK_NO As String = SCANTEXT.Substring(0, 10)
+                        Dim PICK_NO As String = "0" & SCANTEXT.Substring(1, 9)
                         Dim PALLET_SEQ_NO As Integer = CInt(SCANTEXT.Substring(10, 3))
                         Dim PICK_NO_USL As String = SCANTEXT.Substring(13)
                         Dim SCAN_DATE As DateTime = Now
